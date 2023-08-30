515235ac13d0ccab6e106d92d8642c0761206c30
Арслан Гебеков <arslangebekov@icloud.com>
2023-08-30 23:39:29 +0300

    feat: добавлены PaymentProcessor

---
+++ src/External/PaymentProcessor/StripePaymentProcessor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\External\PaymentProcessor;
+
+class StripePaymentProcessor
+{
+    /**
+     * @return bool true if payment was succeeded, false otherwise
+     */
+    public function processPayment(int $price): bool
+    {
+        if ($price < 100) {
+            return false;
+        }
+
+        //process payment logic
+        return true;
+    }
+}
