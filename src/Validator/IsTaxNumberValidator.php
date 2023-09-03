5d26521fdc1d71b5e8325c40554cc133aa059426
Арслан Гебеков <arslangebekov@icloud.com>
2023-09-03 14:04:31 +0300

    refactor: исправлено описание массива с паттернами tax номеров

--- src/Validator/IsTaxNumberValidator.php
+++ src/Validator/IsTaxNumberValidator.php
@@ -10,8 +10,8 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
 class IsTaxNumberValidator extends ConstraintValidator
 {
     /**
-     * @param array<string, string> $countriesTaxPatterns список кодов стран и регулярных выражений, которые должны
-     *                                         проходить проверку: [ 'FR' => '/$FR[A-Z]{2}[0-9]{9}^/' ]
+     * @param array<string, string> $countriesTaxPatterns список регулярных выражений, которые должны
+     *                                                    проходить проверку: [ '/$FR[A-Z]{2}[0-9]{9}^/' ]
      */
     public function __construct(
         private readonly array $countriesTaxPatterns
